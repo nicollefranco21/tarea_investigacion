ea77928e26eb81f20ebbe36d733795c5856c65a3
David Hernandez Mejia <davidmejia0951@gmail.com>
2026-03-25 08:37:22 -0500

    solucion uno terminada

--- tarea_punto_1.py
+++ tarea_punto_1.py
@@ -26,8 +26,8 @@ prob += 3000*X1 + 2500*X2 + 6000*X3 + 2000*X4 + 5000*X5 + 1000*X6 <= 10500, "Añ
 prob += 1000*X1 + 3500*X2 + 4000*X3 + 1500*X4 + 1000*X5 + 500*X6 <= 7000, "Año_2"
 prob += 4000*X1 + 3500*X2 + 5000*X3 + 1800*X4 + 4000*X5 + 900*X6 <= 8750, "Año_3"
 
-# Resolver el problema
-prob.solve()
+# Resolver el problema sin mostrar salida del solver
+prob.solve(pulp.PULP_CBC_CMD(msg=False))
 
 # Mostrar resultados
 print("Estado de la solución:", pulp.LpStatus[prob.status])
